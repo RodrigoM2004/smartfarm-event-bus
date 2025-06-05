1efe1ce708d36eb9e15d66abdb222ade44509b7b
Rodrigo Morales <rodrigo.monasterios@outlook.com>
2025-06-05 15:53:10 -0300

    update(URLs das rotas): altera urls das rotas para as urls das APIs do deploy no heroku

--- src/app.js
+++ src/app.js
@@ -19,25 +19,25 @@ app.use("/event", async (req, res) => {
   const event = req.body;
   // User
   try {
-    await axios.post("http://localhost:3000/event", event);
+    await axios.post(" https://smartfarm-user-mss-899314332d02.herokuapp.com/event", event);
   } catch (e) {
     console.log(e);
   }
   //Sensor
   try {
-    await axios.post("http://localhost:3001/event", event);
+    await axios.post(" https://smartfarm-sensor-mss-60ba36d99b7f.herokuapp.com/event", event);
   } catch (e) {
     console.log(e);
   }
   //Leitura
   try {
-    await axios.post("http://localhost:3002/event", event);
+    await axios.post("https://smartfarm-reading-mss-ee062958e049.herokuapp.com/event", event);
   } catch (e) {
     console.log(e);
   }
   //View
   try {
-    await axios.post("http://localhost:3003/event", event);
+    await axios.post(" https://smartfarm-view-mss-753d9b4258f1.herokuapp.com/event", event);
   } catch (e) {
     console.log(e);
   }
